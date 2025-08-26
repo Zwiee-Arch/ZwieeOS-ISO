c04ee7a39a1c4485248baab7c235caca36a4fa30
crystalforceix <crystalforceix@gmail.com>
2025-08-26 08:20:07 +0700

    add power manager service and use gdm at login manager

--- ISO-Builder/Zwiee_Arch-Core/releng/airootfs/etc/skel/Scripts/install-and-enable-tuned-power-manager.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hey let's me install some dependancy packages of tuned power manager and enable it for you"
-sudo pacman -Sy && sudo pacman -S tuned tuned-ppd
-echo "Done! let's me enable it!"
-sudo systemctl enable tuned tuned-ppd
